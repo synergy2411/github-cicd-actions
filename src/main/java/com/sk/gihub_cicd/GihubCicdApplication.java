5f843e9a0e261daf26edadd7d9dbcdb63c57ce86
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-05 15:23:22 +0530

    api endpoint added

--- src/main/java/com/sk/gihub_cicd/GihubCicdApplication.java
+++ src/main/java/com/sk/gihub_cicd/GihubCicdApplication.java
@@ -2,10 +2,17 @@ package com.sk.gihub_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GihubCicdApplication {
 
+	@GetMapping("/greet")
+	public String sayHello(){
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GihubCicdApplication.class, args);
 	}
