be656106c621bc2e69ee62be6669aefd280d244d
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-05 15:20:41 +0530

    initial commit

---
+++ src/main/java/com/sk/gihub_cicd/GihubCicdApplication.java
@@ -0,0 +1,13 @@
+package com.sk.gihub_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GihubCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GihubCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/gihub_cicd/GihubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.gihub_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GihubCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
